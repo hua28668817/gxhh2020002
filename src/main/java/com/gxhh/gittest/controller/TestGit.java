f9c75d800712c46307c2a6c432bb4418388830fd
lizhonghua <28668817@qq.com>
2020-11-12 22:47:50 +0800

    test git 202011

--- src/main/java/com/gxhh/gittest/controller/TestGit.java
+++ src/main/java/com/gxhh/gittest/controller/TestGit.java
@@ -3,6 +3,7 @@ package com.gxhh.gittest.controller;
 public class TestGit {
 
     public static void main(String[] args) {
+        //是打印，测试提交
         System.out.println("hello git !!");
     }
 }
